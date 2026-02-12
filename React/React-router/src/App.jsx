132c07aea4f5e7c57a7233f6069a65911af206db
25r1116osawa-bit <25r1116.osawa@gmail.com>
2026-02-12 15:45:39 +0900

    ファイルを更新

--- React/React-router/src/App.jsx
+++ React/React-router/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { PrimaryButton } from './components/Button/primaryButton'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <PrimaryButton/>
     </>
   )
 }
---
+++ React/React-router/src/components/Button/PrimaryButton.jsx
@@ -0,0 +1,12 @@
+
+
+export  const PrimaryButton = () =>{
+
+  return (
+    <>
+     <button >送信</button>
+    </>
+  )
+}
+
+export default App
