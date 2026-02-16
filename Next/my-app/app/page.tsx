ea753fd8edf30bf9a09d07704a08c2ebeee6c068
saori <free.and.else@gmail.com>
2026-02-16 20:27:23 +0900

    ファイルを更新

--- Next/my-app/app/page.tsx
+++ Next/my-app/app/page.tsx
@@ -4,5 +4,6 @@ export default function Home() {
   console.log(process.env.DB_NAME);
   return (
     <div>a</div>
+
   );
 }
---
+++ Next/my-app/app/sample/page.tsx
@@ -0,0 +1,6 @@
+export default function Todo() {
+  console.log(process.env.DB_NAME);
+  return (
+    <div>b</div>
+  );
+}
